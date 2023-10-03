408d096321cf367dace664f548ad70e4a0f78596
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-10-03 11:09:21 -0300

    conexion

--- universidadEjemplo/src/UniversidadEjemplo/universidadEjemplo.java
+++ universidadEjemplo/src/UniversidadEjemplo/universidadEjemplo.java
@@ -5,7 +5,9 @@
 package UniversidadEjemplo;
 
 import java.sql.Connection;
-
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Mateo
@@ -14,11 +16,21 @@ public class universidadEjemplo {
 
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
       Connection con;
       con= Conexion.getConexion();
       
+      //Agregar Alumno.
+      String sql="INSERT INTO alumno(dni,apellido,nombre,fechaNacimiento,estado)"+"values(1234,'Lopez','Maria',2000-10-25',true)";
+      PreparedStatement ps=con.prepareStatement(sql);
+     
+      int filas=ps.executeUpdate();
+      if(filas>0){
+          JOptionPane.showMessageDialog(null, "alumno agregado");
+      }
+              
       
     }
     
