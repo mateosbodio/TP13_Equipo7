1f38b2763a0ca952867b2e335a327620479b459d
Julian Rios <julianelsensei@gmail.com>
2023-10-05 12:43:01 -0300

    Update Conexion.java

--- universidadEjemplo/src/UniversidadEjemplo/Conexion.java
+++ universidadEjemplo/src/UniversidadEjemplo/Conexion.java
@@ -48,5 +48,54 @@ public class Conexion {
         
         return con;
     }
+    /*
+    public static void setData(String consulta, String mje) {
+
+        Connection con = null;
+        Statement sta = null;
+        try {
+            con = Conexion.getConnection();
+            sta = con.createStatement();
+            sta.executeUpdate(consulta);
+            if (!mje.equals("")) {
+                JOptionPane.showMessageDialog(null, mje);
+            }
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, e);
+        } finally {
+
+        }
+        try {
+            con.close();
+            sta.close();
+        } catch (Exception e) {
+
+        }
+    }
+    */
+    
+    /*
+    public static ResultSet getResultSet(String consulta){
+        
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        
+        try {
+            con = Conexion.getConnection();
+            st = con.createStatement();
+            rs = st.executeQuery(consulta);
+            return rs;
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+        
+    }
+    
+    */
     
 }
